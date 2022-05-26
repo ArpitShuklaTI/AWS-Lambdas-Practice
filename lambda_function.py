8a6f234c6c824d21554cb08eb9feffcfe1587e9f
arpit <arpitshukla62@gmail.com>
2022-05-26 09:48:25 +0530

    Setup Amazon Comprehend

--- lambda_function.py
+++ lambda_function.py
@@ -3,16 +3,29 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
+    comprehend = boto3.client('comprehend')
+    sns = boto3.client('sns')
+    topic_arn = 'arn:aws:sns:us-east-1:280022023954:shuklaarpit'
     if event:
-        print(f"Event: {event}")
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = event['Records'][0]['s3']['object']['key']
-        print(f"File {key} uploaded in bucket {bucket}")
         try:
-            response = s3.get_object(Bucket=bucket, Key=key)
-            print(f"Response: {response}")
-            file_content = response["Body"].read().decode('utf-8')
-            print(f"File content: {file_content}")
+            file_object = s3.get_object(Bucket=bucket, Key=key)
+            text = file_object["Body"].read().decode('utf-8')
+            sentiment_response = comprehend.detect_sentiment(Text=text, LanguageCode='en')
+            sentiment = sentiment_response["Sentiment"]
+            print(sentiment_response)
+            print(sentiment)
+            key_phrases_response = comprehend.detect_key_phrases(Text=text, LanguageCode='en')
+            key_phrases = [phrase['Text'] for phrase in key_phrases_response['KeyPhrases']]
+            print(key_phrases_response)
+            print(key_phrases)
+            msg = f"""
+            Analysis of "{key}" file added in s3 bucket:
+            Sentiment = {sentiment}
+            Key Phrases = {key_phrases}
+            """
+            sns.publish(TopicArn = topic_arn, Message = msg)
         except Exception as e:
             print(e)
             print(f"Error getting object {key} from bucket {bucket}.")
